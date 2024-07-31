d7e148dbad7a84689e21d8019fb8ba82a2afab06
patrick10jane <[200~^[[200~patrickjane058135@gmail>
2024-07-31 07:26:53 +0400

    'update'

--- src/features/Spl.jsx
+++ src/features/Spl.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function Spl() {
   return (
       <Spline
-      scene="https://prod.spline.design/xYzjE3rhEJTt0WGz/scene.splinecode"
+      scene="https://prod.spline.design/7y4WihuHOycttXWj/scene.splinecode"
       />
   );
 }
