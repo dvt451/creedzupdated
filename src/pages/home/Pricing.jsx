5105b022ad715d80544de4e067d9a0f2d9dc54a0
patrick10jane <[200~^[[200~patrickjane058135@gmail>
2024-08-01 19:12:00 +0400

    'update'

--- src/pages/home/Pricing.jsx
+++ src/pages/home/Pricing.jsx
@@ -196,7 +196,7 @@ export default function Pricing() {
                         modules={[Navigation, Parallax]}
                         spaceBetween={50}
                         speed={1200}
-                        direction='vertical'
+                        parallax
                         onSwiper={(swiper) => setSwiperInstance(swiper)}
                         onSlideChange={handleSlideChange}
                      >
