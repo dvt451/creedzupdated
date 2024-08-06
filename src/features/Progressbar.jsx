f1f704f92793d894d43582f66562cde983def8a0
patrick10jane <[200~^[[200~patrickjane058135@gmail>
2024-08-06 09:20:42 +0400

    'update'

--- src/features/Progressbar.jsx
+++ src/features/Progressbar.jsx
@@ -3,15 +3,16 @@ import React, { useEffect } from 'react'
 import { gsap } from 'gsap';
 
 export default function Progressbar() {
-   if(window.innerWidth > 991.98){
 
    useEffect(() => {
       const progressBar = document.querySelector('.progress-bar__filled');
 
       const updateProgressBar = () => {
+         if(window.innerWidth > 991.98){
             const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
             gsap.to(progressBar, { top: `${scrollPercentage}%`, duration: 0.2 });
-      };
+         };
+      }
   
   
       window.addEventListener('scroll', updateProgressBar);
@@ -20,7 +21,6 @@ export default function Progressbar() {
         window.removeEventListener('scroll', updateProgressBar);
       };
     }, []);
-   }
 
   return (
     <div className='progress-bar'>
