3756ea523cb98f5a4616a6d272d04aac6548bec7
patrick10jane <[200~^[[200~patrickjane058135@gmail>
2024-09-05 09:38:18 +0400

    update

--- src/shared/hooks/ThemeContext.jsx
+++ src/shared/hooks/ThemeContext.jsx
@@ -27,7 +27,7 @@ export function AppWrapper({ children }) {
     setProjectHover,
     colorState,
     setColorState,
-   //  setLoeader,
+    setLoeader,
     loeader,
     setHoverAnyLink,
     hoverAnyLink,
